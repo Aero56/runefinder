5cad2a210c592c16f5a2e79b796b8a5d676dcb9c
Aaron Beirlaen <aaron.beirlaen@gmail.com>
2023-09-03 22:40:15 +0200

    remove normalize.css
    tailwind already provides this kind of thing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import CloseButton from '@components/CloseButton';
 const Home = () => {
   return (
     <div>
-      <h1>RuneFinder</h1>
+      <h1 className="text-3xl">RuneFinder</h1>
       <CloseButton
         onClick={() => {
           console.log('test');
