f6b461db4359fedaeedd3b5bba05ce1dc6d5ec21
Aaron Beirlaen <aaron.beirlaen@gmail.com>
2023-09-03 23:38:09 +0200

    add basic login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,20 @@
+import { Link } from 'react-router-dom';
+
 const Login = () => {
   return (
-    <div>
-      <h1 className="text-3xl">Log in</h1>
-    </div>
+    <>
+      <Link to="..">Back</Link>
+      <div className="p-6 max-w-sm mx-auto bg-slate-700 rounded-xl shadow-md items-center gap-3">
+        <h1 className="text-3xl text-center mb-4">Log in</h1>
+        <div className="flex flex-col gap-3 mb-4">
+          <label>Username</label>
+          <input id="username"></input>
+          <label>Password</label>
+          <input id="password" type="password"></input>
+        </div>
+        <div className="p-4 w-32 text-center bg-slate-600 mx-auto">Log in </div>
+      </div>
+    </>
   );
 };
 
