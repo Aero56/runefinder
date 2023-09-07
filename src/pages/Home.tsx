87fc11492f30183e1f1164014676e3125bdffd77
Aaron Beirlaen <aaron.beirlaen@gmail.com>
2023-09-07 22:35:14 +0200

    add groups

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,35 @@
+import { Table } from '@/types/supabase';
+import { supabase } from '@api/supabase';
 import { useAuth } from '@contexts/AuthContext';
+import { useCallback, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+
+interface Group extends Table<'groups'> {
+  users: {
+    id: string;
+    username: string;
+  }[];
+}
 
 const Home = () => {
   const { user } = useAuth();
+  const [groups, setGroups] = useState<Group[] | null>(null);
+
+  const getGroups = useCallback(async () => {
+    const { data } = await supabase
+      .from('groups')
+      .select('*, users!users_group_id_fkey(id, username)')
+      .order('updated_at', { ascending: false });
+
+    setGroups(data ?? null);
+  }, []);
+
+  useEffect(() => {
+    getGroups();
+  }, [getGroups, user]);
 
   return (
-    <>
+    <div className="m-4">
       <h1 className="text-3xl">RuneFinder</h1>
 
       {user ? (
@@ -17,7 +41,18 @@ const Home = () => {
           <p className="p-4 w-32 text-center bg-slate-600">Log in </p>
         </Link>
       )}
-    </>
+
+      {groups && (
+        <div className="flex flex-col gap-5 mt-5">
+          {groups.map((group) => (
+            <div className="flex flex-col p-4 rounded bg-slate-700">
+              <p>{group.name}</p>
+              <p>{group.users.map((user) => user.username).join(', ')}</p>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
   );
 };
 
--- src/types/supabase.ts
+++ src/types/supabase.ts
@@ -11,26 +11,69 @@ export type Json =
 export interface Database {
   public: {
     Tables: {
+      groups: {
+        Row: {
+          created_at: string;
+          created_by: string;
+          id: string;
+          name: string;
+          size: number;
+          updated_at: string;
+        };
+        Insert: {
+          created_at?: string;
+          created_by: string;
+          id?: string;
+          name: string;
+          size: number;
+          updated_at?: string;
+        };
+        Update: {
+          created_at?: string;
+          created_by?: string;
+          id?: string;
+          name?: string;
+          size?: number;
+          updated_at?: string;
+        };
+        Relationships: [
+          {
+            foreignKeyName: 'groups_created_by_fkey';
+            columns: ['created_by'];
+            referencedRelation: 'users';
+            referencedColumns: ['id'];
+          }
+        ];
+      };
       users: {
         Row: {
           created_at: string;
+          group_id: string | null;
           id: string;
           stats: Stats;
           username: string;
         };
         Insert: {
           created_at?: string;
+          group_id?: string | null;
           id: string;
           stats?: Stats;
           username: string;
         };
         Update: {
           created_at?: string;
+          group_id?: string | null;
           id?: string;
           stats?: Stats;
           username?: string;
         };
         Relationships: [
+          {
+            foreignKeyName: 'users_group_id_fkey';
+            columns: ['group_id'];
+            referencedRelation: 'groups';
+            referencedColumns: ['id'];
+          },
           {
             foreignKeyName: 'users_id_fkey';
             columns: ['id'];
@@ -54,3 +97,6 @@ export interface Database {
     };
   };
 }
+
+export type Table<T extends keyof Database['public']['Tables']> =
+  Database['public']['Tables'][T]['Row'];
