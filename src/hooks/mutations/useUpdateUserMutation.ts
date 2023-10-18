68719393f658b5d177d1b6e75c4b2320ae193984
Aero56 <aaronbeaaron@gmail.com>
2023-10-19 00:02:52 +0200

    allow joining groups

---
+++ src/hooks/mutations/useUpdateUserMutation.ts
@@ -0,0 +1,30 @@
+import { supabase } from '@api/supabase';
+import { useAuth } from '@contexts/AuthContext';
+import { useMutation } from '@tanstack/react-query';
+
+interface UpdateUserMutationProps {
+  group: string;
+}
+
+const useUpdateUserMutation = () => {
+  const { user } = useAuth();
+
+  return useMutation(async ({ group }: UpdateUserMutationProps) => {
+    if (!user) {
+      throw new Error('You must be logged in to do this.');
+    }
+
+    const { data, error } = await supabase
+      .from('users')
+      .update({ group_id: group })
+      .eq('id', user.id);
+
+    if (error) {
+      throw new Error(error.message);
+    }
+
+    return data;
+  });
+};
+
+export default useUpdateUserMutation;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,10 @@ import useGroupsQuery from '@hooks/queries/useGroupsQuery';
 import { useAuth } from '@contexts/AuthContext';
 import CreateParty from '@components/Dialogs/CreateParty';
 import Select, { Option } from '@components/Select';
-import { useState } from 'react';
+import { MouseEvent, useState } from 'react';
+import useUpdateUserMutation from '@hooks/mutations/useUpdateUserMutation';
+import toast from 'react-hot-toast/headless';
+import { Group } from '@/types/group';
 
 const OPTIONS = [
   { label: 'Option 1', value: '1' },
@@ -22,12 +25,29 @@ const Home = () => {
     order: { column: 'updated_at', options: { ascending: false } },
   });
 
+  const { mutateAsync: updateUser, isLoading: isUpdateUserLoading } =
+    useUpdateUserMutation();
+
   if (isLoading) {
     return <p>Loading...</p>;
   }
 
   const handleChangeFilter = (selected: Option[]) => {
     setSelected(selected);
+  };
+
+  const handleJoinGroup = async (group: Group) => {
+    try {
+      await updateUser({ group: group.id });
+    } catch (error) {
+      if (error instanceof Error) {
+        toast(error.message);
+      }
+      return;
+    }
+
+    toast(`You joined group "${group.name}"!`);
+    navigate(`/group/${group.id}`);
   };
 
   return (
@@ -46,23 +66,40 @@ const Home = () => {
           {groups.map((group) => (
             <button
               key={group.id}
-              className="flex flex-col rounded bg-black-pearl-900 p-4 transition-colors duration-300 hover:bg-gray-900"
+              className="rounded bg-black-pearl-900 p-4 transition-colors duration-300 hover:bg-gray-900"
               onClick={() => navigate(`/group/${group.id}`)}
             >
-              <p>{group.name}</p>
-              <p>
-                {group.users.map((user, i) => (
-                  <span key={user.id}>
-                    {i > 0 && ', '}
-                    <Link
-                      to={`/player/${user.username}`}
-                      className="font-medium text-anzac-400 hover:underline"
-                    >
-                      {user.username}
-                    </Link>
-                  </span>
-                ))}
-              </p>
+              <div className="flex justify-between">
+                <div className="flex flex-col text-left">
+                  <p>{group.name}</p>
+                  <p>
+                    {group.users.map((user, i) => (
+                      <span key={user.id}>
+                        {i > 0 && ', '}
+                        <Link
+                          to={`/player/${user.username}`}
+                          className="font-medium text-anzac-400 hover:underline"
+                        >
+                          {user.username}
+                        </Link>
+                      </span>
+                    ))}
+                  </p>
+                </div>
+                <button
+                  className="btn btn-ghost w-16"
+                  onClick={(event: MouseEvent) => {
+                    event.stopPropagation();
+                    handleJoinGroup(group);
+                  }}
+                >
+                  {!isUpdateUserLoading ? (
+                    'Join'
+                  ) : (
+                    <span className="loading loading-spinner"></span>
+                  )}
+                </button>
+              </div>
             </button>
           ))}
         </div>
