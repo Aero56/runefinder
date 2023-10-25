d360c134c3234163e858e7d4b7ab7efd162deddc
Aero56 <aaronbeaaron@gmail.com>
2023-10-25 22:53:35 +0200

    add basic player profile styling

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -1,4 +1,5 @@
 import { useParams } from 'react-router-dom';
+import { format } from 'date-fns';
 
 import usePlayerQuery from '@hooks/queries/usePlayerQuery';
 
@@ -16,14 +17,33 @@ const Player = () => {
   }
 
   return (
-    <div className="flex flex-col">
-      {username}
-      {player?.stats &&
-        Object.entries(player.stats.skills).map(([key, value]) => (
-          <p key={key}>
-            {key}: {value.level}
-          </p>
-        ))}
+    <div className="container grid grid-cols-1 gap-4 pt-4 sm:grid-cols-5">
+      <div className="col-span-1 rounded-xl bg-black-pearl-900 p-4 sm:col-span-2">
+        <p className="text-center text-xl font-bold">{username}</p>
+        <div className="divider my-1" />
+        <p className="text-xs font-bold">ABOUT ME</p>
+        <p className="text-xs font-bold">
+          {'Member since: '}
+          <span className="font-normal">
+            {format(new Date(player.created_at), 'd MMM yyyy')}
+          </span>
+        </p>
+      </div>
+      <div className="col-span-1 rounded-xl bg-black-pearl-900 sm:col-span-3">
+        <div className="rounded-t-xl border-b-2 border-black-pearl-700 bg-black-pearl-800 p-4 font-semibold">
+          Stats
+        </div>
+        <div className="p-4">
+          <div className="flex flex-col">
+            {player?.stats &&
+              Object.entries(player.stats.skills).map(([key, value]) => (
+                <p key={key}>
+                  {key}: {value.level}
+                </p>
+              ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
