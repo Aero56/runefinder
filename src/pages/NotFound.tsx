ea6ecfdb39fd1780fb7fe910571e7c1f79452ec9
Aero56 <aaronbeaaron@gmail.com>
2023-12-24 01:23:56 +0100

    improve not found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,10 @@
 const NotFound = () => {
   return (
-    <div>
-      <h1>Oops! This page could not be found.</h1>
+    <div className="flex h-[calc(100vh-128px)] items-center justify-center sm:h-[calc(100vh-64px)]">
+      <div className="flex flex-col gap-3 p-4 text-center">
+        <h1 className="text-4xl font-bold text-anzac-400 sm:text-6xl">Oops!</h1>
+        <p className="text-xl sm:text-2xl">This page could not be found.</p>
+      </div>
     </div>
   );
 };
