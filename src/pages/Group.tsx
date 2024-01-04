dbaa3313d0eb8df1299669e0562aed78468ca40d
Aero56 <aaronbeaaron@gmail.com>
2024-01-04 19:53:15 +0100

    add better group not found message

--- src/pages/Group.tsx
+++ src/pages/Group.tsx
@@ -125,7 +125,16 @@ const Group = () => {
   }
 
   if (!group) {
-    return <p>This group does not exist.</p>;
+    return (
+      <div className="flex h-[calc(100vh-128px)] items-center justify-center sm:h-[calc(100vh-64px)]">
+        <div className="flex flex-col gap-3 p-4 text-center">
+          <h1 className="text-4xl font-bold text-anzac-400 sm:text-6xl">
+            Oops!
+          </h1>
+          <p className="text-xl sm:text-2xl">This group does not exist.</p>
+        </div>
+      </div>
+    );
   }
 
   const playersNeeded = group.size - group.users.length;
