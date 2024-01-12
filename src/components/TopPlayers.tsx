00d13c72ad149c1214b59482c6a4f7b40adce062
Aero56 <aaronbeaaron@gmail.com>
2024-01-12 01:23:43 +0100

    add top players loading skeleton

--- src/components/TopPlayers.tsx
+++ src/components/TopPlayers.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import useTopPlayersQuery from 'hooks/queries/useTopPlayersQuery';
 
 const TopPlayers = () => {
-  const { data } = useTopPlayersQuery();
+  const { data, isLoading } = useTopPlayersQuery();
 
   return (
     <div className="h-min w-full rounded-xl bg-black-pearl-900 lg:w-1/3">
@@ -11,36 +11,40 @@ const TopPlayers = () => {
         Top players
       </div>
       <div className="flex flex-col gap-2 p-4">
-        {data?.map((player, index) => {
-          const rank = index + 1;
+        {!isLoading
+          ? data?.map((player, index) => {
+              const rank = index + 1;
 
-          return (
-            <Link
-              to={`/player/${player.id}`}
-              className="flex items-center justify-between gap-2 rounded-xl border-2 border-black-pearl-800 bg-black-pearl-950 p-4 transition-colors hover:bg-black-pearl-950/50"
-            >
-              <div className="flex items-center gap-3">
-                <div
-                  className={`h-5 w-5 rounded bg-anzac-400 text-center text-sm font-semibold text-black-pearl-950 ${
-                    rank === 1
-                      ? 'bg-anzac-400'
-                      : rank === 2
-                        ? 'bg-black-pearl-100'
-                        : rank === 3
-                          ? 'bg-amber-600'
-                          : 'bg-black-pearl-600'
-                  }`}
+              return (
+                <Link
+                  to={`/player/${player.id}`}
+                  className="flex items-center justify-between gap-2 rounded-xl border-2 border-black-pearl-800 bg-black-pearl-950 p-4 transition-colors hover:bg-black-pearl-950/50"
                 >
-                  {rank}
-                </div>
-                <p className="font-medium">{player.user?.username}</p>
-              </div>
-              <p className="font-semibold text-anzac-400">
-                {player.raid_score}
-              </p>
-            </Link>
-          );
-        })}
+                  <div className="flex items-center gap-3">
+                    <div
+                      className={`h-5 w-5 rounded bg-anzac-400 text-center text-sm font-semibold text-black-pearl-950 ${
+                        rank === 1
+                          ? 'bg-anzac-400'
+                          : rank === 2
+                            ? 'bg-black-pearl-100'
+                            : rank === 3
+                              ? 'bg-amber-600'
+                              : 'bg-black-pearl-600'
+                      }`}
+                    >
+                      {rank}
+                    </div>
+                    <p className="font-medium">{player.user?.username}</p>
+                  </div>
+                  <p className="font-semibold text-anzac-400">
+                    {player.raid_score}
+                  </p>
+                </Link>
+              );
+            })
+          : [...Array(3)].map(() => (
+              <div className="skeleton h-[60px] w-full rounded-xl bg-black-pearl-950/60" />
+            ))}
       </div>
     </div>
   );
