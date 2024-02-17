dbda6848801e4692d1ab0c815460b687e9500ada
Aero56 <aaronbeaaron@gmail.com>
2024-02-17 15:58:58 +0100

    update news message
    these are just hardcoded for now

--- src/components/NewsAndUpdates.tsx
+++ src/components/NewsAndUpdates.tsx
@@ -1,3 +1,5 @@
+import { format } from 'date-fns';
+
 const NewsAndUpdates = () => {
   return (
     <div className="h-min w-full rounded-xl bg-black-pearl-900 p-4 lg:w-2/3">
@@ -7,13 +9,20 @@ const NewsAndUpdates = () => {
           <h2 className="text-lg font-medium text-anzac-400">
             RuneFinder released!
           </h2>
-          <p className="text-black-pearl-100/60">TBA</p>
+          <p className="text-black-pearl-100/60">
+            {format(new Date('2024-02-17'), 'P')}
+          </p>
         </div>
         <p className="mt-2">
           It's taken me a while, but it's finally out!
           <br />
           Future updates will be posted here.
         </p>
+        <p className="mt-4">Planned updates:</p>
+        <li className="indent-2">Group chats</li>
+        <li className="indent-2">
+          Automatic discord channels only accessible to group members
+        </li>
       </div>
     </div>
   );
